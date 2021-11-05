86a7e309e480ba673d3757ad060e86dd29beff41
gabrysera <gabrieleserafini10100@gmail.com>
2021-11-05 14:09:36 +0100

    creating pillars connections

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 import math
 
-
+"""
 class Canyon_area(object):
     def __init__(self, x, y, pillars_subset):
         self.number_x = x
@@ -12,6 +12,48 @@ class Canyon_area(object):
 def find_pillars(y_bound, x_bound, max_r, pillars):
     
     return 
+"""
+class Pillar(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.dict = {}
+
+    def set_reachable_node(self, p, dist):
+        #for r in reachable_pillars:
+            #self.dict[r[0]] = r[1]
+        self.dict[p] = dist
+
+def distance(p1,p2):
+    return math.sqrt(pow((p1.x - p2.x),2) + pow((p1.y - p2.y),2))
+
+def min_cost(dist, disks):
+    res = []
+    for d in disks:
+        if d[0]*2 >= dist:
+            res.append(d)
+    res.sort(key=lambda x:x[1])
+    return res[0][1]
+
+def reachable_pillars(p, pillars, threshold, disks):
+    res = []
+    for p0 in pillars:
+        dist = distance(p0, p)
+        if dist <= threshold:
+            res.append((p0,min_cost(dist, disks)))
+            cost = res[-1][1]
+            p0.set_reachable_node(p, cost)
+            p.set_reachable_node(p0, cost)
+    return res
+    #return map(lambda p0: (p0, min_cost(p0, p, disks)) ,filter(lambda p0: distance(p0, p) <= threshold, pillars))
+
+def create_adjacency_matrix(W, pillars, disks, max_r):
+    start_index = 0
+    for p in pillars:
+        start_index += 1
+        reachable = reachable_pillars(p, pillars[start_index:], 2*max_r, disks)
+        #p.set_reachable_nodes(reachable)
+        print(p.dict)
 
 def create_graph(W, pillars, disks):
     #pillar X_i, Y_i can only touch pillars at 
@@ -19,11 +61,10 @@ def create_graph(W, pillars, disks):
     #2) determine if the graph would be sparse or dense
     #3) build adjecency data structure
     #4) djikstra
-    pillars = sorted(pillars)
-    max_x = pillars[-1][0]#max(pillars, key=lambda x:x[0])[0]
-    max_r = max(disks, key=lambda x:x[0])[0]
-    areas = [[Canyon_area(y_area, x_area,find_pillars(y_area, x_area, max_r, pillars)) for x_area in range(int(math.ceil(max_x/(max_r*2))))] for y_area in range(int(math.ceil(W/(max_r*2))))]
-    print(len(areas),len(areas[0]))
+    #pillars = sorted(pillars)
+    disks = sorted(disks)
+    max_r = disks[-1][0]
+    create_adjacency_matrix(W, pillars, disks, max_r)
     return 
 
 def read_input():
@@ -32,7 +73,7 @@ def read_input():
     disks = []
     for i in range(0, number_of_pillars):
         (x_i,y_i) = list(map(int, input().split()))
-        pillars.append((int(x_i),int(y_i)))
+        pillars.append(Pillar(int(x_i),int(y_i)))
 
     for i in range(0, m_kind_of_disks):
         (r_i, c_i) = list(map(int, input().split()))
