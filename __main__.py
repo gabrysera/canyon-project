563176b1712663dc22eea4c57c82f0c7efbbc7a0
dineshk.l <dineshk.l@protonmail.com>
2021-11-08 09:28:51 +0100

    changing comments to docstrings

--- __main__.py
+++ __main__.py
@@ -1,25 +1,30 @@
 import math
 
 class Pillar(object):
-    def __init__(self, x, y):  #thus each pillar object has its x and y coordinates (for its placement on the graph).
+    """ x and y coordinates for each pillar object has its  (for its placement on the graph). """
+    def __init__(self, x, y):  
         self.x = x
         self.y = y
         self.dict = {}
 
-    def set_reachable_node(self, p, possible_disks): #this function can be used to add a reachable pillar object p to the dictionary of nodes accesible to the main pillar object, and the distance associated to reach it.
+    def set_reachable_node(self, p, possible_disks): 
+        """ this function can be used to add a reachable pillar object p to the dictionary of nodes accesible to the main pillar object, and the distance associated to reach it. """
         self.dict[p] = possible_disks
 
-def distance(p1,p2): #given two points, this function returns the (Pythogorean) distance between the two Pillar objects.
+def distance(p1,p2): 
+    """ given two points, this function returns the (Pythogorean) distance between the two Pillar objects."""
     return math.sqrt(pow((p1.x - p2.x),2) + pow((p1.y - p2.y),2))
 
-def possible_disks(dist, disks_pairs): #in this function, we include all the possible disc sizes that culd be used to reach the distance required to the other pillar, and in the end return the size of the cheapest disc.
+def possible_disks(dist, disks_pairs): 
+    "this function includes all the possible disc sizes that could be used to reach the distance required to the other pillar, and in the end return the size of the cheapest disc. """
     possible_disks = []
     for disk in disks_pairs:
         if disk[2] >= dist:
             possible_disks.append(disk)
     return sorted(possible_disks, key=lambda x: x[3])
     
-def reachable_pillars(p, pillars, threshold, disks_pairs): #given a starting pillar, it compares with every other pillar, and if the distance between them is less than the biggest sized disc available(threshold), then its added as a reachable node to both 
+def reachable_pillars(p, pillars, threshold, disks_pairs): 
+    """ given a starting pillar, it compares with every other pillar, and if the distance between them is less than the biggest sized disc available(threshold), then its added as a reachable node to both """
     for p0 in pillars:
         dist = distance(p0, p)
         if dist <= threshold:
@@ -28,7 +33,8 @@ def reachable_pillars(p, pillars, threshold, disks_pairs): #given a starting pil
             p.set_reachable_node(p0, p_p0_pairs)
     #return res #self note: instead of appending to res and returning, what if we dont append to it and return that
 
-def create_adjacency_matrix(W, pillars, disks_pairs, max_r): #this function creates the adjacency list by using dictionaries with each pillar object as key and its values including a nested dictionary of the nodes accessible to it and the cost to access it.
+def create_adjacency_matrix(W, pillars, disks_pairs, max_r): 
+    """ this function creates the adjacency list by using dictionaries with each pillar object as key and its values including a nested dictionary of the nodes accessible to it and the cost to access it. """
     start_index = 0
     for p in pillars:
         start_index += 1
@@ -42,7 +48,8 @@ def disks_combinations(disks):
             combinations.append((d[0],d1[0],d[0]+d1[0],d[1]+d1[1]))
     return combinations
 
-def create_graph(W, pillars, disks): #this function sorts the disks with increasing size and chooses the biggest disc size available as the maximum size to pass as the threshold to the function for creating the adjacency matrix.
+def create_graph(W, pillars, disks): 
+    """ this function sorts the disks with increasing size and chooses the biggest disc size available as the maximum size to pass as the threshold to the function for creating the adjacency matrix. """
     #store pillars optimal distances
     #only store distances, and then while you run the search you add cost depending on the actual disk
     #after a path is found, optimizie it (checks if you can reduce cost) 
@@ -60,7 +67,8 @@ def create_graph(W, pillars, disks): #this function sorts the disks with increas
         print("\n")
     return 
 
-def read_input(): #this function reads the input and stores them into variables and appends others as tuples to lists to be later used in creating a graph representation and running the Dijstra's algorithm on it.
+def read_input(): 
+    """ this function reads the input and stores them into variables and appends others as tuples to lists to be later used in creating a graph representation and running the Dijstra's algorithm on it. """
     number_of_pillars,m_kind_of_disks,y_goal = list(map(int, input().split()))
     pillars = []
     disks = []
